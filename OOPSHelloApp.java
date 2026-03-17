a0950cb5459fe9586f2def9011201d7fb4733b57
sasank2007-30 <sasankreddy30@gmail.com>
2026-03-17 13:39:17 +0530

    uc2 done

--- OOPSHelloApp.java
+++ OOPSHelloApp.java
@@ -1,5 +1,6 @@
 public class OOPSHelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
+        System.out.println("Hello World"); 
+        String name = "sasank"; 
+        System.out.println("Hello, " + name + "!");
+    }}
